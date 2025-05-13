6fcd5e2a60ab1fce6ee77421dc33e6f2c668bb43
Hassaan <imhassaan333@gmail.com>
2025-05-13 09:24:32 +0530

    update 1

--- API/Program.cs
+++ API/Program.cs
@@ -40,7 +40,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 var accessToken = context.Request.Query["access_token"];
 
                 var path = context.HttpContext.Request.Path;
-                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"));
+                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
                 {
                   context.Token = accessToken;
                 }
